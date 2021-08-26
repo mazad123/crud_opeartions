05b26ac08dab39b5adfd623663130e415558130c
Mohammad Azad <mukeshvaidya@zapbuild.com>
2021-08-26 10:49:25 +0530

    crudChanges

---
+++ app/controllers/tutorial.controller.js
@@ -0,0 +1,64 @@
+const db = require("../models");
+const Tutorial = db.tutorials;
+
+// Create and Save a new Tutorial
+exports.create = (req, res) => {
+  // Validate request
+	if (!req.body.title) {
+		res.status(400).send({
+		  message: "Content can not be empty!"
+		});
+		return;
+	  }
+	
+	  // Create a Tutorial
+	  const tutorial = {
+		title: req.body.title,
+		description: req.body.description,
+		published: req.body.published ? req.body.published : false
+	  };
+	
+	  // Save Tutorial in the database
+	  Tutorial.create(tutorial)
+		.then(data => {
+		  res.send(data);
+		})
+		.catch(err => {
+		  res.status(500).send({
+			message:
+			  err.message || "Some error occurred while creating the Tutorial."
+		  });
+		});
+};
+
+
+// Retrieve all Tutorials from the database.
+exports.findAll = (req, res) => {
+	Tutorial.findAll()
+	  .then(data => {
+		res.send(data);
+	  })
+	  .catch(err => {
+		res.status(500).send({
+		  message:
+			err.message || "Some error occurred while retrieving tutorials."
+		});
+	  });
+  };
+
+// Find a single Tutorial with an id
+exports.findOne = (req, res) => {
+	const id = req.params.id;
+  
+	Tutorial.findByPk(id)
+	  .then(data => {
+		res.send(data);
+	  })
+	  .catch(err => {
+		res.status(500).send({
+		  message: "Error retrieving Tutorial with id=" + id
+		});
+	  });
+  };
+
+
